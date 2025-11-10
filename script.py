ef6d74d332fa80e2779db56f85a0116fe1dc160a
Harry Burgess <harry.burgess@tipigroup.com>
2025-11-10 16:35:11 +0000

    update: UI

--- script.py
+++ script.py
@@ -28,7 +28,7 @@ st.markdown("""
    SECTION HEADERS
    ========================= */
 .section-header {
-    background: --roast-yellow;
+    background: --roast-primary;
     border-radius: 10px;
     padding: 15px 20px;
     color: var(--roast-dark);
